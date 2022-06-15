c18a01b206cae5f4c7f677995ceb9d11cfc0dd35
frshipley <frshipley@gmail.com>
2022-06-15 13:43:51 -0400

    added no-sandbox flag to chromedriver

--- modules/scrape_dk.py
+++ modules/scrape_dk.py
@@ -41,6 +41,7 @@ def parse(url):
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--no-sandbox")
 
     if os.name =='nt':
         chrometype = ChromeType.GOOGLE
